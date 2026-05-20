1cef8b855e4231d2d7c369cf633ed27b2c4fd757
PEDROisaias <pedrogomes123md@gmail.com>
2026-05-20 12:47:26 -0300

    Feature: Componente StatusModal construida

---
+++ MyIoTProject/src/components/LightControl.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { MaterialCommunityIcons as Icon } from '@expo/vector-icons';
+
+export default function LightControl({ isLightOn, })
